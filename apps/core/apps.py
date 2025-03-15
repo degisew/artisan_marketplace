e25a9a908cbeed22b49a58c06c4875044d97fe6e
Degisew <jegisew21@gmail.com>
2025-03-15 12:28:36 +0300

    feat: Added a custom user policy loading logic

--- apps/core/apps.py
+++ apps/core/apps.py
@@ -1,6 +1,26 @@
+import json
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.core'
+
+    # Custom code below is used to load access policies once
+    # the django app starts. The design decision is to load
+    # the policy file only once. We can put this code Inside
+    # each access policy class but the leads to loading a
+    # file in every instantiation and that is not necessary.
+    policies = {}
+
+    def ready(self):
+        """Preload policies when the Django app starts."""
+        policy_file_path = getattr(settings, "POLICIES_FILE_PATH", None)
+        if policy_file_path:
+            try:
+                with open(policy_file_path, 'r') as f:
+                    CoreConfig.policies = json.load(f)
+            except (FileNotFoundError, json.JSONDecodeError) as e:
+                raise Exception(f"Error loading policies file: {e}")
+
