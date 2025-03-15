bf6139db938894bc4ef94d2c0523a4317cdf89e3
Degisew <jegisew21@gmail.com>
2025-03-15 12:28:53 +0300

    feat: Applied permissions

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,15 +1,19 @@
-from rest_framework import viewsets
-from rest_framework.permissions import (
-    AllowAny,
-    IsAdminUser,
-    IsAuthenticated
-)
-
+from http import HTTPMethod
+from rest_framework import viewsets, mixins, filters, status
+from django_filters.rest_framework import DjangoFilterBackend
 from apps.core.models import DataLookup, SystemSetting
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from apps.core.permissions import (
+    DataLookupAccessPolicy,
+    SystemSettingAccessPolicy
+)
 from apps.core.serializers import (
     DataLookupSerializer,
     DataLookupTypeSerializer,
-    SystemSettingSerializer
+    ResetSystemSettingSerializer,
+    SystemSettingResponseSerializer,
+    SystemSettingSerializer,
 )
 
 
@@ -33,22 +37,44 @@ class AbstractModelViewSet(viewsets.ModelViewSet):
 
 
 class DataLookupViewSet(AbstractModelViewSet):
-    permission_classes = [AllowAny]
+    permission_classes = [DataLookupAccessPolicy]
     http_method_names = ["get"]
     serializer_class = DataLookupSerializer
     queryset = DataLookup.objects.all()
 
 
-# @extend_schema(tags=["Data Lookup"], request=DataLookupTypeSerializer)
 class DataLookupTypeViewSet(viewsets.ReadOnlyModelViewSet):
-    permission_classes = [IsAuthenticated, IsAdminUser]
+    permission_classes = [DataLookupAccessPolicy]
     pagination_class = None
     queryset = DataLookup.objects.all().distinct("type").order_by("type")
     serializer_class = DataLookupTypeSerializer
 
 
-class SystemSettingViewSet(AbstractModelViewSet):
-    permission_classes = [AllowAny]
-    http_method_names = ["get"]
-    serializer_class = SystemSettingSerializer
+class SystemSettingViewSet(
+    mixins.ListModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet,
+):
+    permission_classes = [SystemSettingAccessPolicy]
     queryset = SystemSetting.objects.all()
+    http_method_names = ["get", "post", "patch"]
+    serializer_class = SystemSettingSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+
+    @action(
+        url_path="reset",
+        detail=True,
+        methods=[HTTPMethod.PATCH],
+        serializer_class=ResetSystemSettingSerializer,
+    )
+    def reset(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(data=request.data, instance=instance)
+        if serializer.is_valid(raise_exception=True):
+            instance.current_value = instance.default_value
+            instance.save()
+            return Response(
+                SystemSettingResponseSerializer(instance).data,
+                status=status.HTTP_200_OK,
+            )
